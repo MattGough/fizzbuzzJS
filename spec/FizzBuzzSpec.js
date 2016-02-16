9aa3570aa0a0a3e6dcae5a91613cd77180131ba5
Reiss Johnson <reissjohnson@me.com>
2016-02-16 10:55:36 +0000

    written two tests for fizz & buzz

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,10 @@
+describe("FizzBuzz", function() {
+  it("returns fizz when passed 3", function() {
+    expect(FizzBuzz(3)).toBe("Fizz");
+  });
+
+  it("returns buzz when passed 5", function() {
+    expect(FizzBuzz(5)).toBe("Buzz");
+  });
+
+});
---
+++ src/FizzBuzz.js
@@ -0,0 +1,14 @@
+function FizzBuzz() {
+
+return "Fizz";
+
+
+
+
+
+
+
+
+
+
+}
